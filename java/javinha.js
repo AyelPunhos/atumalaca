cccb7c10f409707e4e8c01db8bd1d3cf74716829
AyelPunhos <80044091+AyelPunhos@users.noreply.github.com>
2022-03-28 11:22:17 -0300

    Add files via upload

---
+++ java/javinha.js
@@ -0,0 +1,25 @@
+document.write("Ariel")
+document.write("<p> Para começar, ele é um pirata, o que faz muitos pensarem que ele é uma má pessoa, mesmo trabalhando na Baroque Works, ele continua sendo uma pessoa incrível e muito carinhosa. Ao se encontrar pela primeira vez com Monkey D. Luffy, fez rapidamente amizade com ele e com todos os Mugiwaras, e conquistou o amor do bando. Um tempo depois teve que lutar com Sanji mesmo não querendo, e descobriu que eles poderiam ser amigos. Mais um temp depois, em Impel Down, reencontrou Luffy e ajudou ele a resgata ro irmão e a fugir da prisão. Como o último recurso, sacrificou a liberdade dele após se disfarçar de diretor da prisão, e abrir os porões para Luffy, Crocodile, Buggy, Jimbei e Mr. 3. Todo o bando do chapéu de palha jamais se esquecerá de seu sacrifício.</p>");
+var valor1, bb, idade, s1, s2, soma;
+valor1 = 69;
+bb=true;
+document.write('<p>sexo? '+valor1+'</p>');
+
+idade= new Array();
+idade[0]=15;
+idade[1]=16;
+idade[2]=17;
+document.write('<h1>Idade aluno 1 '+idade[1]+'</h1>');
+
+alert('ATUMALACA'+idade[2]);
+
+valor1=prompt('melhor time do interior de minas gerais: ')
+document.write('<h1> valor mano '+valor1 +'</h1>' );
+
+bb=confirm('deseja cocaína? ');
+document.write('<p>o fdp pikou: '+bb+'</p>');
+
+s1=prompt('digite camisa bruno henrique: ');
+s2=prompt('digite numero gabigo: ');
+soma=parseInt(s1)+parseInt(s2);
+document.write('<h1>goleiro cassio: '+soma+'</h1>');
